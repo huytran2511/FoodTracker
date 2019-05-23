838a310fc8e487eeb8296eff1890c066f59f9e15
Huy Tran <tranquanghuy999@gmail.com>
2019-05-23 11:28:41 -0700

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by user154893 on 5/23/19.
+//  Copyright © 2019 Huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
